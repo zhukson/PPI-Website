9b47fcaa343561ca3a6b34d05add1c61ed70d70a
zhukson <1739173986@qq.com>
2024-05-05 04:14:21 -0400

    update

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+import Navigator from "../src/components/Navigator.vue";
+import ContactUs from "../src/components/ContactUs.vue";
+
+const router = useRouter();
+const route = useRoute();
+</script>
+
+<template>
+  <div class="main">
+    <Navigator></Navigator>
+    <RouterView></RouterView>
+    <ContactUs></ContactUs>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+}
+</style>
