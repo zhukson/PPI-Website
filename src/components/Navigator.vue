16aff47311fe174f2e05aeb4e1a335a5d13131cc
zhukson <1739173986@qq.com>
2024-05-11 20:48:37 -0400

    update

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useRouter } from "vue-router";
 import Dropdown from "primevue/dropdown";
+import { ref } from "vue";
 import { lang } from "../helpers/language";
 
 const router = useRouter();
 const goHome = () => {
   router.push({ path: "/" });
+};
+const showMenu = ref(false);
+
+const toggleMenu = (status) => {
+  showMenu.value = status;
 };
 </script>
 
@@ -28,7 +34,33 @@ const goHome = () => {
       <div class="item" @click="router.push({ path: '/about' })">
         {{ lang === "CN" ? "关于" : "About" }}
       </div>
-      <div class="item">{{ lang === "CN" ? "解决方案" : "Solutions" }}</div>
+      <div class="item-dropdown" @mouseover="toggleMenu(true)">
+        {{ lang === "CN" ? "解决方案" : "Solutions" }}
+        <div class="mega-menu" v-if="showMenu" @mouseleave="toggleMenu(false)">
+          <div class="photo-row">
+            <div class="card">
+              <img src="../assets/navigator/electronic.png" class="image" />
+              <div>Consuming Electronics</div>
+            </div>
+            <div class="card">
+              <img src="../assets/navigator/industry.png" class="image" />
+              <div>Industry</div>
+            </div>
+            <div class="card">
+              <img src="../assets/navigator/telecom.png" class="image" />
+              <div>Telecommunication</div>
+            </div>
+            <div class="card">
+              <img src="../assets/navigator/automotive.png" class="image" />
+              <div>Automotive Component</div>
+            </div>
+            <div class="card">
+              <img src="../assets/navigator/industry.png" class="image" />
+              <div>Medical</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="item">
         {{ lang === "CN" ? "质量" : "Quality Assurance" }}
       </div>
@@ -57,7 +89,12 @@ const goHome = () => {
     margin-right: 2rem;
     align-items: center;
     .item {
-      font-size: 1.5rem;
+      font-size: 1rem;
+      font-weight: light;
+    }
+
+    .item-dropdown {
+      font-size: 1rem;
       font-weight: light;
     }
   }
@@ -66,6 +103,51 @@ const goHome = () => {
 .item:hover {
   cursor: pointer;
   color: rgb(255, 230, 0);
+}
+
+.item-dropdown:hover {
+  color: rgb(255, 230, 0);
+}
+
+.card:hover {
+  cursor: pointer;
+}
+
+.mega-menu {
+  position: absolute;
+  width: 100%;
+  height: 35%;
+  left: 0;
+  right: 0;
+  top: 5rem;
+  background-color: white; /* Change as per your design */
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* Optional */
+  z-index: 100;
+}
+
+.photo-row {
+  padding: 3rem;
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: space-evenly;
+  align-items: center;
+  color: black;
+
+  .card {
+    display: flex;
+    flex-direction: column;
+    font-size: 1.5rem;
+    font-weight: 600;
+    align-items: center;
+    justify-content: center;
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  }
+
+  .image {
+    height: 12rem;
+    width: 18rem;
+  }
 }
 
 @media (max-width: 768px) {
--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 
 import HomeView from '../src/pages/main/index.vue'
 import About from '../src/pages/about/index.vue'
+import Solution from '../src/pages/solutions/index.vue'
 
 const routes = [
   { path: '/', component: HomeView },
   { path: '/about', component: About },
+  { path: '/solutions', component: Solution}
 ]
 
 export const router = createRouter({
